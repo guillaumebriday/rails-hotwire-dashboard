a57aa3c1356cb43f64a108793e920a52ab8467ed
Guillaume Briday <guillaumebriday@gmail.com>
2022-11-05 15:52:34 +0100

    Renaming Dashboard service

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
   def index
     @orders = Order.by_date(params[:by_date]).order(:created_at)
 
-    service = Dashboard.new(@orders)
+    service = DashboardService.new(@orders)
 
     @cumulative_orders_total = service.cumulative_orders_total
     @order_items_quantity = service.order_items_quantity
--- app/services/dashboard_service.rb
+++ app/services/dashboard_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Dashboard
+class DashboardService
   def initialize(orders)
     @orders = orders
   end
